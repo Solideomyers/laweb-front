d99db6b5d738b5944ce386222a2d844413088e2c
Solideomyers <franciscomyers@gmail.com>
2023-12-21 19:46:56 +0100

    FIx: modificacion del archivo de barril que contiene todos los componentes reutilizables de la ui

---
+++ client/src/components/ui/index.js
@@ -0,0 +1,6 @@
+export { Input } from "./Input.tsx";
+export { Button } from "./Button.tsx";
+export { ButtonSecondary } from "./ButtonSecondary.tsx";
+export { Label } from "./Label.tsx";
+export { ComboBoxUi } from "./ComboBoxUi.tsx";
+export { CarouselUi } from "./Carousel.tsx";
