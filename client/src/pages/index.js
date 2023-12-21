ba9d786f1a40653086f77486e117218ce1b4f332
Solideomyers <franciscomyers@gmail.com>
2023-12-21 19:47:37 +0100

    FIx: modificacion del archivo de barril que contiene todas las paginas

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,1 +1,4 @@
-export { HomePage } from "./home/HomePage.jsx";
+export { HomePage } from "./home/HomePage.tsx";
+export { CategoriaPage } from "./categoriapage/CategoriaPage.tsx";
+export { ProductoPage } from "./productopage/ProductoPage.tsx";
+export { NotfoundPage } from "./notfoundpage/NotfoundPage.tsx";
