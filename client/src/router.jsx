96ffc43cafc3ea38f143d79587e4ddc664d1033f
Solideomyers <franciscomyers@gmail.com>
2023-12-21 19:50:25 +0100

    Fix: modificacion del router, el cual se encarga unicamente de manejar las rutas de la aplicacion, hasta este momento cuenta con las rutas de home donde se muestran todas las categorias, la de los productos de cada categoria y la ruta del producto seleccionado

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import { HomePage } from "pages";
+import { CategoriaPage, HomePage, ProductoPage, NotfoundPage } from "pages";
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -12,6 +12,20 @@ export const router = createBrowserRouter([
         element: <HomePage />,
         id: "home",
       },
+      {
+        path: "/categoria/:id",
+        element: <CategoriaPage />,
+        id: "categoriabyid",
+      },
+      {
+        path: "/categoria/:id/producto/:idp",
+        element: <ProductoPage />,
+        id: "productobyidp",
+      },
     ],
+  },
+  {
+    path: "*",
+    element: <NotfoundPage />,
   },
 ]);
