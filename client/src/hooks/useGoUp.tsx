273c95389c6bb065b41cf6bee83b95e53f1c07f7
Solideomyers <franciscomyers@gmail.com>
2023-12-21 19:29:16 +0100

    Fix: estilos del button hook modificados

--- client/src/hooks/useGoUp.tsx
+++ client/src/hooks/useGoUp.tsx
@@ -17,8 +17,8 @@ export const useGoUp = () => {
     <div className="fixed bottom-10 right-4 z-50 rounded-full p-0  text-white text-md  font-bold animate-keep-bounce">
       {showButton && (
         <button
-          className="text-white bg-primary py-1 px-1 shadow-sm rounded-full hover:scale-125 transform-gpu transition-transform ease-in-out duration-104 delay-104"
-          onClick={() => window.scrollTo(0, 0)}
+          className="relative py-1 px-1 text-black text-base font-bold uppercase rounded-[50px] overflow-hidden bg-white transition-all duration-400 ease-in-out shadow-md shadow-neutral-900/60 hover:scale-125 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-blue-500 before:to-blue-300 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-[50px] hover:before:left-0"
+          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
         >
           <CaretCircleDoubleUp size={25} />
         </button>
