0babe544aeaf7dea81fdc2ccbcf83d842b4448dc
Solideomyers <franciscomyers@gmail.com>
2024-01-17 13:42:21 +0100

    fix: added types

--- client/src/components/producto/Producto.tsx
+++ client/src/components/producto/Producto.tsx
@@ -1,75 +1,82 @@
-"use client";
-import { Badge, Button, Card, PlayButton, Progress } from "keep-react";
-import { Heart, ShoppingCart, MagnifyingGlass } from "phosphor-react";
-import React from "react";
-import img from "../../assets/herosection.jpg";
-import { Link } from "react-router-dom";
-interface Data {
-  price: number;
-  name: string;
-  description: string;
-  id: string;
-  idp: number;
-  offer: boolean;
-}
+'use client';
+import { Badge, Card } from 'keep-react';
+import { Button } from '../ui';
+import { Heart, ShoppingCart, MagnifyingGlass } from 'phosphor-react';
+import React from 'react';
+import img from '../../assets/herosection.jpg';
+import { Link } from 'react-router-dom';
+import { useProductContext } from '@/context/ProductContext';
+import { Product } from '@/interfaces/interfaces';
 
-export const Producto: React.FC<Data> = ({
-  price,
-  name,
+export const Producto: React.FC<Product> = ({
+  attribute_price,
+  proname,
   idp,
-  id,
+  id_o,
+  id_category,
   offer,
   description,
+  cat_name,
 }) => {
-  // para tomar el primer parrafo de la descripcion
-  const indexDescription = description.indexOf(".");
+  const indexDescription = description.indexOf('.');
   const textDescription = description.slice(0, indexDescription + 1);
+  const { products, setProducts } = useProductContext();
+
+  const handleAddProduct = () => {
+    const updateProduct: Product = {
+      id_category,
+      proname,
+      attribute_price,
+      idp,
+      id_o,
+      description,
+      cat_name,
+    };
+    console.log(cat_name);
+    console.log('click');
+    setProducts([...products, updateProduct]);
+  };
+
   return (
     <>
       <Card
-        className="max-w-sm h-[400px] overflow-hidden rounded-md hover:shadow-lg animate-reveal transition-all ease-in-out duration-300"
+        className='size-30 hover:scale-y-105 overflow-hidden rounded-md hover:shadow-lg transition-transform ease-in-out duration-300'
         imgSrc={img}
-        imgSize="md"
+        imgSize='md'
       >
-        <Card.Container className="absolute right-3.5 top-3.5 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-metal-50/50">
-          <Heart size={20} weight="bold" color="white" />
+        <Card.Container className='absolute right-3.5 top-3.5 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-metal-50/50'>
+          <Heart size={20} weight='bold' color='white' />
         </Card.Container>
-        <Card.Container className="p-6">
-          <Card.Container className="flex items-center justify-between">
+        <Card.Container className='p-6 flex flex-col items-center'>
+          <Card.Container className='flex items-center justify-between w-full'>
             <Badge
-              size="xs"
-              colorType="light"
-              color={`${offer ? "error" : "gray"}`}
+              size='xs'
+              colorType='light'
+              color={`${offer ? 'error' : 'gray'}`}
             >
-              {offer ? "Oferta" : "Actualizado"}
+              {offer ? 'Oferta' : 'Actualizado'}
             </Badge>
-            <Card.Title>{price}$</Card.Title>
+            <Card.Title>Desde {attribute_price}$</Card.Title>
           </Card.Container>
-          <Card.Container className="my-3">
-            <Card.Title>{name}</Card.Title>
+          <Card.Container className='my-3'>
+            <Card.Title>{proname}</Card.Title>
             <Card.Description>
               <span
                 dangerouslySetInnerHTML={{ __html: textDescription }}
-                className="grid grid-cols-1 grid-rows-1 font-light text-base whitespace-pre-wrap truncate overflow-hidden h-[60px] "
+                className='grid grid-cols-1 grid-rows-1 font-light text-sm whitespace-pre-wrap truncate overflow-hidden h-[60px] '
               />
             </Card.Description>
           </Card.Container>
-          <Card.Container className="flex items-center justify-between gap-5">
-            <Link to={`/producto/${id}`}>
-              <Button size="sm" type="outlineGray">
-                <span className="pr-2">
-                  <ShoppingCart size={24} />
-                </span>
-                Añadir
-              </Button>
-            </Link>
-            <Link to={`/categoria/${id}/producto/${idp}`}>
-              <Button
-                size="sm"
-                type={"linkGray"}
-                className="bg-primary text-white border border-neutral-900/20"
-              >
-                <span className="pr-2">
+          <Card.Container className='flex items-center justify-between gap-5'>
+            <Button onClick={handleAddProduct} variant={'primary'}>
+              <span className='pr-2'>
+                <ShoppingCart size={24} />
+              </span>
+              Añadir
+            </Button>
+            <Link to={`/categoria/${id_category}/producto/${idp}/${id_o}`}>
+              <Button variant={'secondary'}>
+                <span className='pr-2'>
                   <MagnifyingGlass size={24} />
                 </span>
                 Detalle
