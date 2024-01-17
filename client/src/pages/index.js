af40dd65101f5b58628dabf7b03e59bfcc0012ec
Solideomyers <franciscomyers@gmail.com>
2024-01-17 13:49:46 +0100

    added pages

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,3 +2,4 @@ export { HomePage } from "./home/HomePage.tsx";
 export { CategoriaPage } from "./categoriapage/CategoriaPage.tsx";
 export { ProductoPage } from "./productopage/ProductoPage.tsx";
 export { NotfoundPage } from "./notfoundpage/NotfoundPage.tsx";
+export {ContactPage} from "./contact/ContactPage.tsx"
