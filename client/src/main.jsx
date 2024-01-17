f7fffba738a3eac4ed6395cf20e35328b18e4e0d
Solideomyers <franciscomyers@gmail.com>
2024-01-17 13:58:44 +0100

    fix: refactor

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,24 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
-import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { store, persistor } from "@/redux";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import {queryClient} from "../react-query.ts";
 import { router } from "./router.jsx";
 import { Breadcrumbs } from "components";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <QueryClientProvider client={new QueryClient()}>
+        <QueryClientProvider client={queryClient}>
+          <ReactQueryDevtools initialIsOpen={ true} />
           <RouterProvider router={router}>
             <Breadcrumbs />
           </RouterProvider>
         </QueryClientProvider>
-      </PersistGate>
-    </Provider>
   </React.StrictMode>
 );
