bfc7944af476cbfc6df3784d5b806375051cac4e
Solideomyers <franciscomyers@gmail.com>
2024-01-17 13:58:30 +0100

    fix: refactor

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,32 @@
 import { Outlet, useLocation } from "react-router-dom";
-import { Banner, Breadcrumbs, NavBar } from "./components";
-import { useGoUp } from "./hooks";
+import { Banner, Breadcrumbs, Footer, NavBar } from "./components";
+import { useFloatBtn } from "./hooks";
+import { CaretCircleDoubleUp } from 'phosphor-react'
+import { createContext } from "react";
+import { ProductProvider } from "./context/ProductContext";
+export const contextStorage = createContext()
 function App() {
   const location = useLocation();
-  const goUp = useGoUp();
-  // console.log(location);
+  const goUp = useFloatBtn({icon: <CaretCircleDoubleUp size={25}/>, variant:"scroll", contenedor: false})
+  
   return (
+    <ProductProvider>
+
     <section>
-      <nav className="z-50 sticky top-0 flex flex-col gap-0">
+      <nav className='z-50 sticky top-0 flex flex-col gap-0'>
         <Banner />
         <NavBar />
       </nav>
-      <main className="grid">
-        {location.pathname !== "/" ? <Breadcrumbs /> : null}
+      <main className="grid grid-cols-1 mx-auto px-2">
+        {location.pathname !== '/' ? <Breadcrumbs /> : null}
         <Outlet />
       </main>
-      <footer></footer>
+      <footer>
+        <Footer />
+      </footer>
       {goUp}
     </section>
-  );
+    </ProductProvider>
+  )
 }
 export default App;
