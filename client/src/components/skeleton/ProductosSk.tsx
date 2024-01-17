185fc18051d792854fd8028ffba90c30e919bca8
Solideomyers <franciscomyers@gmail.com>
2024-01-17 12:59:34 +0100

    feat: loading one card

---
+++ client/src/components/skeleton/ProductosSk.tsx
@@ -0,0 +1,18 @@
+'use client';
+import React from 'react';
+import { ProductoSk } from 'components';
+
+export const ProductosSk: React.FC = () => {
+  return (
+    <section className='w-full inline-flex flex-wrap gap-2 justify-center'>
+      <ProductoSk />
+      <ProductoSk />
+      <ProductoSk />
+      <ProductoSk />
+      <ProductoSk />
+      <ProductoSk />
+      <ProductoSk />
+      <ProductoSk />
+    </section>
+  );
+};
