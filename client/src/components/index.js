84a0d2b0ecfdb22fa995df466a1e61a77358b047
Solideomyers <franciscomyers@gmail.com>
2024-01-17 13:37:16 +0100

    wip: add components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,9 +3,26 @@ export { Banner } from "./banner/Banner.tsx";
 export { Producto } from "./producto/Producto.tsx";
 export { Categoria } from "./categoria/Categoria.tsx";
 export { ProductoDetail } from "./producto/ProductoDetail.tsx";
+export { SeccionDetailR } from "./producto/SeccionDetailR.tsx";
 export { Notfound } from "./notfound/Notfound.tsx";
 export { Breadcrumbs } from "./breadcrumbs/Breadcrumbs.tsx";
 export { Categorias } from "./categoria/Categorias.tsx";
 export { PaginationUi } from "./pagination/PaginationUi.tsx";
 export { Loading } from "./loading/Loading.tsx";
 export { Productos } from "./producto/Productos.tsx";
+export { Accordeon } from "./accordeon/Accordeon.tsx";
+export { Hero } from "./hero/Hero.tsx";
+export { CardHero } from "./hero/CardHero.tsx";
+export { ProductosSlide } from "./producto/ProductosSlide.tsx"
+export { ProductoSk } from "./skeleton/ProductoSk.tsx"
+export { ProductosSk } from './skeleton/ProductosSk.tsx';
+export { Testimonials } from "./testimonials/Testimonials.tsx"
+export { Testimonial } from "./testimonials/Testimonial.tsx"
+export { Footer } from "./footer/Footer.tsx"
+export {FooterPie} from "./footer/FooterPie.tsx"
+export { Enlaces } from './footer/Enlaces.tsx'
+export { FooterHead } from './footer/FooterHead.tsx'
+export { Tabinfo } from "./tabinfo/Tabinfo.tsx"
+export { ProductosRelated } from "./producto/ProductosRelated.tsx"
+export { Cart } from "./cart/Cart.tsx"
+export {Form} from "./contact/Form.tsx"
