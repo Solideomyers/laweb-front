b278f2a8c87d7715b1860836b38e0b7534761d8b
Solideomyers <franciscomyers@gmail.com>
2024-01-18 12:36:49 +0100

    fix: refactor useCategorias, export categoriasQuery and import apirURL

--- client/src/hooks/query/useCategorias.ts
+++ client/src/hooks/query/useCategorias.ts
@@ -4,7 +4,7 @@ import {
 } from '@/interfaces/categorias/interfaces-categorias';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-const apiUrl = import.meta.env.VITE_API_URL_BASE;
+import { apiUrl } from '../../../react-query';
 
 const fetchCategorias: CategoriasQueryFn = async () => {
   const { data } = await axios.get<CategoriaRes>(`${apiUrl}/categorias`);
@@ -12,11 +12,36 @@ const fetchCategorias: CategoriasQueryFn = async () => {
 };
 
 export const useCategorias = () => {
-  return useQuery<CategoriaRes>({
+  const categoriasQuery = useQuery<CategoriaRes>({
     queryKey: ['categorias'],
     queryFn: fetchCategorias,
-    gcTime: 1000 * 60 * 10,
-    staleTime: 1000 * 60 * 5,
-    refetchOnWindowFocus: true,
+    refetchOnWindowFocus: false,
+    initialData: {
+      messageType: 'success',
+      message: 'Resultados solicitados.',
+      totalcategorias: 20,
+      data: [
+        {
+          id: 1,
+          name: 'Colchones',
+        },
+        {
+          id: 9,
+          name: 'Cabeceros',
+        },
+        {
+          id: 281,
+          name: 'Cabeceros Combinados',
+        },
+        {
+          id: 50,
+          name: 'Canapés Abatibles',
+        },
+      ],
+    },
   });
+
+  return {
+    categoriasQuery,
+  };
 };
